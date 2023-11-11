28aaa964be6f68cb32d252c39f8cb4e7673dddbb
martincurtet <55143244+martincurtet@users.noreply.github.com>
2023-11-11 14:56:39 -0500

    feat: router
    - added router in index.js
    - deleted App.js
    - created Home.js
    - created Battle.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Battle from './pages/Battle'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/:battleuuid' element={<Battle />}/>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 )
