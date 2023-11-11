4fd4064e66f4a2d2232ff1c73965a8b2e93167a9
martincurtet <55143244+martincurtet@users.noreply.github.com>
2023-11-11 15:38:07 -0500

    feat: socket.io
    - added socket.io connection
    - added connection button on the home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { socket } from '../connections/socket'
 
 const Home = () => {
+  const createRoom = () => {
+    socket.connect()
+  }
+
   return (
-    <div>
-      Home
+    <div className='page-home'>
+      <h1>Tacitus Wargaming</h1>
+      <button
+        onClick={createRoom}
+      >
+        Create Room
+      </button>
     </div>
   )
 }
