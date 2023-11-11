958c05c1ce894c64a485a81b6f017ef4ed8df180
martincurtet <55143244+martincurtet@users.noreply.github.com>
2023-11-11 16:48:10 -0500

    feat: username and join
    - added username modal input
    - added socket connection on choosing a username

--- src/pages/Battle.js
+++ src/pages/Battle.js
@@ -1,9 +1,54 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { socket } from '../connections/socket'
+import Modal from '../components/Modal'
 
 const Battle = () => {
+  const params = useParams()
+
+  // USERNAME VARIABLES
+  const [username, setUsername] = useState('')
+  const [isUsernameModalOpen, setIsUsernameModalOpen] = useState(true)
+  const [inputUsername, setInputUsername] = useState('Tacitus')
+
+  // USERNAME FUNCTIONS
+  const changeInputUsername = (e) => {
+    if (/^[a-zA-Z0-9]*$/.test(e.target.value)) {
+      setInputUsername(e.target.value)
+    }
+  }
+
+  const submitUsernameModal = () => {
+    if(inputUsername.length > 0 && inputUsername.length <= 20) {
+      setUsername(inputUsername)
+      setInputUsername('')
+      setIsUsernameModalOpen(false)
+    }
+  }
+
+  useEffect(() => {
+    if (username !== '') {
+      if (!socket.connected) {
+        socket.connect()
+      }
+      socket.emit('join-room', { uuid: params.battleuuid, username: username })
+    }
+  }, [username, params])
+
   return (
     <div>
-      Battle
+      <Modal
+        isOpen={isUsernameModalOpen}
+        hasCancel={false}
+        onSubmit={submitUsernameModal}
+      >
+        <div>Choose a username</div>
+        <input
+          type='text'
+          value={inputUsername}
+          onChange={changeInputUsername}
+        />
+      </Modal>
     </div>
   )
 }
