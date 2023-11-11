f50368f916be0b97069906585775d5ad1697b23e
martincurtet <55143244+martincurtet@users.noreply.github.com>
2023-11-11 16:00:46 -0500

    feat: room creation
    - added emit create-room socket event
    - added receive room-created socket event
    - added navigate to battle page after room creation

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 import { socket } from '../connections/socket'
 import '../styles/pages/Home.css'
 
 const Home = () => {
+  const navigate = useNavigate()
+
   const createRoom = () => {
     socket.connect()
+    socket.emit('create-room')
   }
+
+  useEffect(() => {
+    socket.on('room-created', (data) => {
+      navigate(`/${data.uuid}`)
+    })
+
+    return () => {
+      socket.off('room-created')
+    }
+  }, [navigate])
 
   return (
     <div className='page-home'>
