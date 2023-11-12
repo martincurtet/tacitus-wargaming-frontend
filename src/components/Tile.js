249938c02791de398bc68f8bed19b831c8241ea7
martincurtet <55143244+martincurtet@users.noreply.github.com>
2023-11-12 18:55:55 -0500

    feat: tile
    - removed unnecessary code
    - added simpler code for this component

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -1,17 +1,13 @@
 import React from 'react'
 import '../styles/components/Tile.css'
-import { integerToLetter } from '../functions/functions'
 
-const Tile = ({ rowIndex, columnIndex }) => {
-  const isTableHeader = rowIndex === 0 && columnIndex === 0
-  const isRowHeader = columnIndex === 0
-  const isColumnHeader = rowIndex === 0
-
-  const tileContent = isTableHeader ? '' : isRowHeader ? rowIndex : isColumnHeader ? (columnIndex) : ''
-
+const Tile = ({ content, color }) => {
   return (
-    <div className='tile'>
-      {tileContent}
+    <div
+      className='tile'
+      style={{ backgroundColor: color }}
+    >
+      {content}
     </div>
   )
 }
