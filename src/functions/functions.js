fd10b4e66388404ed7f7557f7c1da969ccc86d6b
martincurtet <55143244+martincurtet@users.noreply.github.com>
2023-11-13 14:38:04 -0500

    feat: cell range function
    - added cellRange functions returning a list of cells in a rectangle selection

--- src/functions/functions.js
+++ src/functions/functions.js
@@ -22,7 +22,34 @@ const terrainToHex = (t) => {
   return match[t]
 }
 
+const cellRange = (start, end) => {
+  if (start == end) return [start]
+  const parseCoordinates = (coord) => {
+    const match = coord.match(/([A-Z]+)([0-9]+)/)
+    const [, column, row] = match
+    const columnNumeric = column.split('').reduce((acc, char) => acc * 26 + char.charCodeAt(0) - 64, 0)
+    return { column: columnNumeric, row: parseInt(row, 10)}
+  }
+
+  const startCell = parseCoordinates(start)
+  const endCell = parseCoordinates(end)
+
+  const smallerColumn = startCell.column < endCell.column ? startCell.column : endCell.column
+  const largerColumn = startCell.column >= endCell.column ? startCell.column : endCell.column
+  const smallerRow = startCell.row < endCell.row ? startCell.row : endCell.row
+  const largerRow = startCell.row >= endCell.row ? startCell.row : endCell.row
+
+  const result = []
+  for (let r = smallerRow; r <= largerRow; r++) {
+    for (let c = smallerColumn; c <= largerColumn; c++) {
+      result.push(`${integerToLetter(c)}${r}`)
+    }
+  }
+  return result
+}
+
 module.exports = {
   integerToLetter,
-  terrainToHex
+  terrainToHex,
+  cellRange
 }
