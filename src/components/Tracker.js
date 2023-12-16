f5207601de72f5a4bbee8d6158b4fd98efa939fb
martincurtet <55143244+martincurtet@users.noreply.github.com>
2023-12-16 08:35:57 -0500

    refac: unitmanager
    - moved unit manager modal from Units to Tracker

--- src/components/Tracker.js
+++ src/components/Tracker.js
@@ -1,15 +1,190 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { socket } from '../connections/socket'
+import Modal from './Modal'
 import Units from './Units'
 
-const Tracker = ({ setBoard, factionShop, setFactionShop, factions, setFactions, unitShop, units, setUnits, setLog }) => {
-  // TODO move some stuff back here instead of units
-  // modal unit manager
-  // units -> unit component (not here)
-  // toolbar
+import { DndContext } from '@dnd-kit/core'
+import Draggable from './dndComponents/Draggable'
+import Droppable from './dndComponents/Droppable'
+
+const Tracker = ({
+    setBoard,
+    factionShop, setFactionShop,
+    factions, setFactions,
+    unitShop,
+    units, setUnits,
+    setLog
+  }) => {
+
+  //
+  const params = useParams()
+
+  // UNIT MANAGER VARIABLES
+  const [isUnitManagerModalOpen, setIsUnitManagerModalOpen] = useState(false)
+  const [unitManagerUnits, setUnitManagerUnits] = useState([])
+
+  // UNIT MANAGER FUNCTIONS
+  const openUnitManagerModal = () => {
+    setIsUnitManagerModalOpen(true)
+  }
+  
+  const closeUnitManagerModal = () => {
+    setIsUnitManagerModalOpen(false)
+  }
+
+  const cancelUnitManagerModal = () => {
+    setUnitManagerUnits(units)
+    closeUnitManagerModal()
+  }
+
+  const submitUnitManagerModal = () => {
+    socket.emit('update-units', { uuid: params.battleuuid, units: unitManagerUnits })
+    closeUnitManagerModal()
+  }
+
+  const handleDragEnd = (e) => {
+    const { over } = e
+    if (over === null) return
+    addUnitToFaction(e.active.id, over.id)
+  }
+
+  const addUnitToFaction = (unitCode, factionCode) => {
+    let duplicateUnits = unitManagerUnits.filter(
+      u => u.code.split('-').slice(0, 2).join('-') === `${factionCode}-${unitCode}`
+    )
+
+    let code = `${factionCode}-${unitCode}`
+    let identifier = ''
+    if (duplicateUnits.length === 1) {
+      // add A to this unit
+      setUnitManagerUnits((prev) =>
+        prev.map((u) =>
+          u.code === `${factionCode}-${unitCode}` ? {...u, code: `${u.code}-A`, identifier: 'A' } : u
+        )
+      )
+      // add B to the other one
+      code = `${code}-B`
+      identifier = 'B'
+    } else if (duplicateUnits.length > 1) {
+      // order the array
+      duplicateUnits.sort((a, b) => {
+        if (a.identifier < b.identifier) return -1
+        if (a.identifier > b.identifier) return 1
+        return 0
+      })
+      // find last id and get next one
+      let lastIdentifier = duplicateUnits[duplicateUnits.length - 1].identifier
+      identifier = String.fromCharCode(lastIdentifier.charCodeAt(0) + 1)
+      code = `${code}-${identifier}`
+    }
+
+    setUnitManagerUnits(prev => {
+      let currentUnit = unitShop.find(u => u.code === unitCode)
+      let currentFaction = factions.find(f => f.code === factionCode)
+      return [
+        ...prev,
+        {
+          code: code,
+          name: currentUnit.name,
+          veterancy: currentUnit.veterancy,
+          identifier: identifier,
+          faction: currentFaction.name,
+          icon: currentUnit.icon,
+          men: '20', // default value?
+          hdPerMen: currentUnit.hdPerMen,
+          maxHd: parseInt(currentUnit.hdPerMen) * 20,
+          hd: parseInt(currentUnit.hdPerMen) * 20,
+          casualties: '0',
+          fatigue: '0',
+          notes: ''
+        },
+      ]
+    })
+  }
+
+  const menChange = (code, men) => {
+    // console.log(code, men)
+    setUnitManagerUnits((prev) =>
+      prev.map((u) =>
+        u.code === code ? {...u, men: men, maxHd: men * u.hdPerMen, hd: men * u.hdPerMen } : u
+      )
+    )
+  }
+
+  useEffect(() => {
+    setUnitManagerUnits(units)
+  }, [units])
+
+  useEffect(() => {
+    socket.on('units-updated', (data) => {
+      setUnits(data.units)
+      setBoard(data.board)
+      setLog(data.log)
+    })
+
+    return () => {
+      socket.off('units-updated')
+    }
+  }, [])
 
   return (
     <div>
+      <button onClick={openUnitManagerModal}>Unit Manager</button>
       <Units factionShop={factionShop} setFactionShop={setFactionShop} setLog={setLog} setBoard={setBoard} factions={factions} unitShop={unitShop} units={units} setUnits={setUnits} />
+    
+      <Modal
+        isOpen={isUnitManagerModalOpen}
+        onCancel={cancelUnitManagerModal}
+        onSubmit={submitUnitManagerModal}
+       >
+        <DndContext onDragEnd={handleDragEnd}>
+          <div className='unit-store'>
+            {unitShop.map((u) => (
+              <Draggable id={u.code} key={u.code}>
+                <div className='unit-item' tooltip={`${u.name}\nHD per men: ${u.hdPerMen}`}>
+                  {u.icon ? (
+                    <img src={require(`../images/${u.icon}`)} alt='' height={32} width={32} />
+                  ) : null}
+                </div>
+              </Draggable>
+            ))}
+          </div>
+          <button>Add Faction</button>
+          <div className='faction-panels'>
+            {factions.map((f) => (
+              <Droppable key={f.code} id={f.code}>
+                <div className='faction-panel'>
+                  <div className='faction-panel-title'>{f.name}</div>
+                  {unitManagerUnits.map((u) => {
+                    if (u.faction === f.name) {
+                      return (
+                        // <Draggable id={`${f.name}${u.name}`} key={u.name}>
+                        <div key={u.code} className='faction-unit'>
+                          {u.name} {u.identifier}
+                          <div>
+                          <input
+                            type='number'
+                            value={u.men}
+                            onChange={(e) => menChange(u.code, parseInt(e.target.value))}
+                          /> men {u.maxHd} HD <button>x</button></div>
+                        </div>
+                        // </Draggable>
+                      )
+                    }
+                  })}
+                </div>
+              </Droppable>
+            ))}
+          </div>
+          {/* <button>Add a faction</button>
+          <select>
+            {factionShop.map((f) => (
+              <option>{f.name}</option>
+            ))}
+          </select> */}
+        </DndContext>
+      </Modal>
     </div>
   )
 }
--- src/components/Units.js
+++ src/components/Units.js
@@ -1,12 +1,7 @@
-import React, { useEffect, useState } from 'react'
-import Modal from './Modal'
+import React, { useEffect } from 'react'
 import { socket } from '../connections/socket'
 import { useParams } from 'react-router-dom'
 import { debounce } from '../functions/functions'
-
-import { DndContext } from '@dnd-kit/core'
-import Draggable from './dndComponents/Draggable'
-import Droppable from './dndComponents/Droppable'
 
 import '../styles/components/Units.css'
 
@@ -16,103 +11,12 @@ const Units = ({ factionShop, setFactionShop, setLog, setBoard, factions, unitSh
   //
   const params = useParams()
 
-  // UNIT MANAGER VARIABLES
-  const [isUnitManagerModalOpen, setIsUnitManagerModalOpen] = useState(false)
-  const [unitManagerUnits, setUnitManagerUnits] = useState([])
-
   // const veterancies = [
   //   { id: 0, name: 'Militia', icon: require('../images/militia.png'), next: 1 },
   //   { id: 1, name: 'Normal', icon: require('../images/normal.png'), next: 2 },
   //   { id: 2, name: 'Veteran', icon: require('../images/veteran.png'), next: 3 },
   //   { id: 3, name: 'Elite', icon: require('../images/elite.png'), next: 0 }
   // ]
-
-  const openUnitManagerModal = () => {
-    setIsUnitManagerModalOpen(true)
-  }
-  
-  const closeUnitManagerModal = () => {
-    setIsUnitManagerModalOpen(false)
-  }
-
-  const cancelUnitManagerModal = () => {
-    setUnitManagerUnits(units)
-    closeUnitManagerModal()
-  }
-
-  const submitUnitManagerModal = () => {
-    socket.emit('update-units', { uuid: params.battleuuid, units: unitManagerUnits })
-    closeUnitManagerModal()
-  }
-
-  const handleDragEnd = (e) => {
-    const { over } = e
-    if (over === null) return
-    addUnitToFaction(e.active.id, over.id)
-  }
-
-  const addUnitToFaction = (unitCode, factionCode) => {
-    let duplicateUnits = unitManagerUnits.filter(
-      u => u.code.split('-').slice(0, 2).join('-') === `${factionCode}-${unitCode}`
-    )
-
-    let code = `${factionCode}-${unitCode}`
-    let identifier = ''
-    if (duplicateUnits.length === 1) {
-      // add A to this unit
-      setUnitManagerUnits((prev) =>
-        prev.map((u) =>
-          u.code === `${factionCode}-${unitCode}` ? {...u, code: `${u.code}-A`, identifier: 'A' } : u
-        )
-      )
-      // add B to the other one
-      code = `${code}-B`
-      identifier = 'B'
-    } else if (duplicateUnits.length > 1) {
-      // order the array
-      duplicateUnits.sort((a, b) => {
-        if (a.identifier < b.identifier) return -1
-        if (a.identifier > b.identifier) return 1
-        return 0
-      })
-      // find last id and get next one
-      let lastIdentifier = duplicateUnits[duplicateUnits.length - 1].identifier
-      identifier = String.fromCharCode(lastIdentifier.charCodeAt(0) + 1)
-      code = `${code}-${identifier}`
-    }
-
-    setUnitManagerUnits(prev => {
-      let currentUnit = unitShop.find(u => u.code === unitCode)
-      let currentFaction = factions.find(f => f.code === factionCode)
-      return [
-        ...prev,
-        {
-          code: code,
-          name: currentUnit.name,
-          veterancy: currentUnit.veterancy,
-          identifier: identifier,
-          faction: currentFaction.name,
-          icon: currentUnit.icon,
-          men: '20', // default value?
-          hdPerMen: currentUnit.hdPerMen,
-          maxHd: parseInt(currentUnit.hdPerMen) * 20,
-          hd: parseInt(currentUnit.hdPerMen) * 20,
-          casualties: '0',
-          fatigue: '0',
-          notes: ''
-        },
-      ]
-    })
-  }
-
-  const menChange = (code, men) => {
-    // console.log(code, men)
-    setUnitManagerUnits((prev) =>
-      prev.map((u) =>
-        u.code === code ? {...u, men: men, maxHd: men * u.hdPerMen, hd: men * u.hdPerMen } : u
-      )
-    )
-  }
 
   const hdChange = (code, hd) => {
     console.log(code, hd)
@@ -171,30 +75,18 @@ const Units = ({ factionShop, setFactionShop, setLog, setBoard, factions, unitSh
   }
 
   useEffect(() => {
-    setUnitManagerUnits(units)
-  }, [units])
-
-  useEffect(() => {
-    socket.on('units-updated', (data) => {
-      setUnits(data.units)
-      setBoard(data.board)
-      setLog(data.log)
-    })
-
     socket.on('unit-updated', (data) => {
       setUnits(data.units)
       setLog(data.log)
     })
 
     return () => {
-      socket.off('units-updated')
       socket.off('unit-updated')
     }
   }, [])
 
   return (
     <div>
-      <button onClick={openUnitManagerModal}>Unit Manager</button>
       <div>
       {units.map((u, i) => (
         <div key={u.code}>
@@ -240,54 +132,7 @@ const Units = ({ factionShop, setFactionShop, setLog, setBoard, factions, unitSh
       ))}
       </div>
 
-      <Modal
-        isOpen={isUnitManagerModalOpen}
-        onCancel={cancelUnitManagerModal}
-        onSubmit={submitUnitManagerModal}
-       >
-        <DndContext onDragEnd={handleDragEnd}>
-          <div className='unit-store'>
-            {unitShop.map((u) => (
-              <Draggable id={u.code} key={u.code}>
-                <div className='unit-item' tooltip={`${u.name}\nHD per men: ${u.hdPerMen}`}>
-                  {u.icon ? (
-                    <img src={require(`../images/${u.icon}`)} alt='' height={32} width={32} />
-                  ) : null}
-                </div>
-              </Draggable>
-            ))}
-          </div>
-          <div className='faction-panels'>
-            {factions.map((f) => (
-              <Droppable key={f.code} id={f.code}>
-                <div className='faction-panel'>
-                  <div className='faction-panel-title'>{f.name}</div>
-                  {unitManagerUnits.map((u) => {
-                    if (u.faction === f.name) {
-                      return (
-                        // <Draggable id={`${f.name}${u.name}`} key={u.name}>
-                        <div key={u.code} className='faction-unit'>
-                          {/* <button onClick={() => veterancyChange(u.code)}> */}
-                            {/* <img src={veterancies[u.veterancy].icon} height={16} width={16} /> */}
-                          {/* </button> */}
-                          {u.name} {u.identifier}
-                          <div>
-                          <input
-                            type='number'
-                            value={u.men}
-                            onChange={(e) => menChange(u.code, parseInt(e.target.value))}
-                          /> men {u.maxHd} HD <button>x</button></div>
-                        </div>
-                        // </Draggable>
-                      )
-                    }
-                  })}
-                </div>
-              </Droppable>
-            ))}
-          </div>
-        </DndContext>
-      </Modal>
+      
     </div>
   )
 }
