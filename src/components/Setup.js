e4fc5aeefecab444a85616b3d0fdbd44de6dafc6
martincurtet <55143244+martincurtet@users.noreply.github.com>
2024-07-05 17:52:55 -0400

    feat: setup component
    - created Setup.js file
    - added logic for step navigation, local state for now and first step (factions)
    - added css layout

---
+++ src/components/Setup.js
@@ -0,0 +1,72 @@
+import React, { useState } from 'react'
+import Factions from './Factions'
+import Button from './Button'
+
+import '../styles/components/Setup.css'
+
+const Setup = ({ step, users, factions }) => {
+  //
+  const [stateStep, setStateStep] = useState(1)
+
+  //
+  const stepTitles = {
+    1: 'Factions',
+    2: 'Units',
+    3: 'Initiative',
+    4: 'Board'
+  }
+
+  const renderStepContent = () => {
+    switch (stateStep) {
+      case 1:
+        return (
+          <Factions
+            users={users}
+            factions={factions}
+          />
+        )
+      case 2:
+        return (
+          <div>Units</div>
+        )
+      case 3:
+        return (
+          <div>Initiative</div>
+        )
+      case 4:
+        return (
+          <div>Board</div>
+        )
+      default:
+        return (
+          <div>Error - no step</div>
+        )
+    }
+  }
+
+  const prevStep = () => {
+    setStateStep(prev => prev - 1)
+  }
+
+  const nextStep = () => {
+    setStateStep(prev => prev + 1)
+  }
+
+  // RENDER
+  return (
+    <div className='setup'>
+      <div>stepper steps and stuff</div>
+      <div className='setup-box'>
+        <div className='setup-title'>{stepTitles[stateStep]}</div>
+        {renderStepContent()}
+        {/* <button onClick={prevStep}>Back</button> */}
+        <div className='setup-buttons'>
+          <Button onClick={nextStep}>Confirm</Button>
+          <Button onClick={prevStep}>Back</Button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Setup
