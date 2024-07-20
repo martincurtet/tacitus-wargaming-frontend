f724c337b249b97a38d30d686f0a2cdb963305f7
martincurtet <55143244+martincurtet@users.noreply.github.com>
2024-07-19 23:04:57 -0400

    feat: selected unit
    - added state and function to determine current unit to assign
    - added temporary visual indicators for origin row, the unit and destination rows

--- src/components/SetupInitiative.js
+++ src/components/SetupInitiative.js
@@ -1,28 +1,72 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import UnitIcon from './UnitIcon'
 
 import '../styles/components/SetupInitiative.css'
 
 const SetupInitiative = ({ units, setUnits, factions }) => {
+  //
+  const [currentUnitIndex, setCurrentUnitIndex] = useState(0)
+
+  const selectNextUnitIndex = () => {
+    let selectedIndex = -1
+    let lowestInitiativeRaw = Infinity
+    for (let i = 0; i < units.length; i++) {
+      const unit = units[i]
+      if (unit.initiative === null && unit.initiativeRaw < lowestInitiativeRaw) {
+        lowestInitiativeRaw = unit.initiativeRaw
+        selectedIndex = i
+      } else if (unit.initiative === null && unit.initiativeRaw === lowestInitiativeRaw) {
+        if (i > selectedIndex) {
+          selectedIndex = i
+        }
+      }
+    }
+    return selectedIndex
+  }
+
+  useEffect(() => {
+    setCurrentUnitIndex(selectNextUnitIndex())
+  }, [units])
+
+  const assignNewInitiative = (value) => {
+    console.log(` new initiative ${parseInt(value)}`)
+    // send socket event
+  }
+
   // RENDER FUNCTIONS
   const renderTable = (start, end, step, origin) => {
     let tableRows = []
     for (let i = start; (step > 0 ? i < end : i > end); i += step) {
+      const currentUnit = units[currentUnitIndex]
+      let factionStratAbility = factions.find(f => f.code === currentUnit.factionCode).stratAbility
+      let isHighlighted = (origin && currentUnit.initiativeRaw === i) ||
+        (
+          !origin &&
+          i >= parseInt(currentUnit.initiativeRaw - factionStratAbility) &&
+          i <= parseInt(currentUnit.initiativeRaw + factionStratAbility)
+        )
       let unitImages = units
-        .filter(u => origin ? u.initiativeRaw === i : u.initiative === i)
-        .map(u => (
+      .filter(u => origin ? u.initiativeRaw === i : u.initiative === i)
+      .map(u => {
+        const uniqueIdentifier = `${u.factionCode}-${u.unitCode}-${u.identifier}`
+        return (
           <UnitIcon
             className={'initiative-row-image'}
             tooltip={`${u.name}\n${u.men} Men`}
             unitIconName={u.iconName}
             factionIconName={factions.find(f => f.code === u.factionCode).icon}
             veterancyIconName={'militia.png'}
+            highlighted={uniqueIdentifier === `${currentUnit.factionCode}-${currentUnit.unitCode}-${currentUnit.identifier}`}
           />
-        ))
+        )})
       tableRows.push(
-        <tr key={i} className={`${i % 2 === 0 ? 'even' : 'odd'}`}>
+        <tr
+          key={i}
+          className={`${i % 2 === 0 ? 'even' : 'odd'}`}
+          onClick={() => assignNewInitiative(i)}
+        >
           <td className='row-header'>{i}</td>
-          <td className='row-units'>{unitImages}</td>
+          <td className={`row-units ${isHighlighted ? 'highlighted' : ''}`}>{unitImages}</td>
         </tr>
       )
     }
@@ -39,8 +83,13 @@ const SetupInitiative = ({ units, setUnits, factions }) => {
   // RENDER
   return (
     <div className='setup-initiative'>
-      {renderTable(20, 0, -1, true)}
-      {renderTable(25, -5, -1, false)}
+      <div className='current-unit'>
+        Current Unit Information {units[currentUnitIndex]?.factionCode}-{units[currentUnitIndex]?.unitCode}-{units[currentUnitIndex]?.identifier}
+      </div>
+      <div className='initiative-tables'>
+        {renderTable(20, 0, -1, true)}
+        {renderTable(25, -5, -1, false)}
+      </div>
     </div>
   )
 }
--- src/components/UnitIcon.js
+++ src/components/UnitIcon.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 import '../styles/components/UnitIcon.css'
 
-const UnitIcon = ({ className, tooltip, unitIconName, factionIconName, veterancyIconName }) => {
+const UnitIcon = ({ className, tooltip, unitIconName, factionIconName, veterancyIconName, highlighted }) => {
   return (
-    <div className={`unit-image-container ${className}`} tooltip={tooltip}>
+    <div className={`unit-image-container ${className} ${highlighted ? 'highlighted' : ''}`} tooltip={tooltip}>
       <img src={require(`../images/${unitIconName}`)} alt='' className='unit-image' />
       <img src={require(`../images/${factionIconName}`)} alt='' className='faction-image' />
       <img src={require(`../images/${veterancyIconName}`)} alt='' className='veterancy-image' />
