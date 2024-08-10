0fd1f17da504ca77db0073d22f39da4e4ce824af
martincurtet <55143244+martincurtet@users.noreply.github.com>
2024-08-10 16:53:18 -0400

    feat: unit type
    - updated initiative step to work with unit types instead of units
    - fixed issue where you could click on a row not highlighted

--- src/components/SetupInitiative.js
+++ src/components/SetupInitiative.js
@@ -11,7 +11,9 @@ const SetupInitiative = ({ users, units, setUnits, factions }) => {
   //
   const params = useParams()
   const [user, setUser] = useContext(UserContext)
-  const [currentUnitIndex, setCurrentUnitIndex] = useState(0)
+  const [currentUnitTypeIndex, setCurrentUnitTypeIndex] = useState(0)
+
+  const unitTypes = [...new Set(units.map(unit => `${unit.factionCode}-${unit.unitCode}`))]
 
   const veterancyMap = {
     0: {
@@ -28,35 +30,33 @@ const SetupInitiative = ({ users, units, setUnits, factions }) => {
     }
   }
 
-  const selectNextUnitIndex = () => {
+  const selectNextUnitTypeIndex = () => {
     let selectedIndex = -1
     let lowestInitiativeRaw = Infinity
-    for (let i = 0; i < units.length; i++) {
-      const unit = units[i]
-      if (unit.initiative === null && unit.initiativeRaw < lowestInitiativeRaw) {
-        lowestInitiativeRaw = unit.initiativeRaw
-        selectedIndex = i
-      } else if (unit.initiative === null && unit.initiativeRaw === lowestInitiativeRaw) {
-        if (i > selectedIndex) {
-          selectedIndex = i
-        }
+    unitTypes.forEach((type, index) => {
+      const [factionCode, unitCode] = type.split('-')
+      const unitGroup = units.filter(unit => unit.factionCode === factionCode && unit.unitCode === unitCode)
+      const initiativeRaw = Math.min(...unitGroup.map(unit => unit.initiativeRaw))
+
+      if (unitGroup.some(unit => unit.initiative === null) && initiativeRaw < lowestInitiativeRaw) {
+        lowestInitiativeRaw = initiativeRaw
+        selectedIndex = index
       }
-    }
+    })
     return selectedIndex
   }
 
   useEffect(() => {
-    setCurrentUnitIndex(selectNextUnitIndex())
+    setCurrentUnitTypeIndex(selectNextUnitTypeIndex())
   }, [units])
 
   const assignNewInitiative = (value) => {
-    // check that user faction is correct
-    if (user.isHost || user.userFaction === units[currentUnitIndex].factionCode) {
+    const [factionCode, unitCode] = unitTypes[currentUnitTypeIndex].split('-')
+    if (user.isHost || user.userFaction === factionCode) {
       socket.emit('change-initiative', {
         roomUuid: params.battleuuid,
-        factionCode: units[currentUnitIndex].factionCode,
-        unitCode: units[currentUnitIndex].unitCode,
-        identifier: units[currentUnitIndex].identifier,
+        factionCode: factionCode,
+        unitCode: unitCode,
         initiative: parseInt(value)
       })
     }
@@ -70,42 +70,55 @@ const SetupInitiative = ({ users, units, setUnits, factions }) => {
     return () => {
       socket.off('initiative-changed')
     }
-  }, [])
+  }, [socket, setUnits])
 
   // RENDER FUNCTIONS
   const renderTable = (start, end, step, origin) => {
     let tableRows = []
     for (let i = start; (step > 0 ? i < end : i > end); i += step) {
-      let unitImages = null
+      let unitImages = []
       let isHighlighted = false
-      const currentUnit = units[currentUnitIndex]
-      let factionStratAbility = factions.find(f => f.code === currentUnit?.factionCode)?.stratAbility
-      isHighlighted = (origin && currentUnit?.initiativeRaw === i) ||
-        (
-          !origin &&
-          i >= parseInt(currentUnit?.initiativeRaw - factionStratAbility) &&
-          i <= parseInt(currentUnit?.initiativeRaw + factionStratAbility)
-        )
-      unitImages = units
-      .filter(u => origin ? u.initiativeRaw === i : u.initiative === i)
-      .map(u => {
-        const uniqueIdentifier = `${u.factionCode}-${u.unitCode}-${u.identifier}`
-        return (
-          <UnitIcon
-            className={'initiative-row-image'}
-            tooltip={`${u.name}\n${u.men} Men`}
-            unitIconName={u.iconName}
-            factionIconName={factions.find(f => f.code === u.factionCode).icon}
-            veterancyIconName={veterancyMap[u.veterancy].iconName}
-            highlighted={uniqueIdentifier === `${currentUnit?.factionCode}-${currentUnit?.unitCode}-${currentUnit?.identifier}`}
-          />
-        )})
+
+      const currentUnitType = unitTypes[currentUnitTypeIndex]
+      const [factionCode, unitCode] = currentUnitType?.split('-') || ['', '']
+      let factionStratAbility = factions.find(f => f.code === factionCode)?.stratAbility
+      const unitGroup = units.filter(u => u.factionCode === factionCode && u.unitCode === unitCode)
+      
+      isHighlighted = unitGroup.some(unit => 
+        (origin && unit.initiativeRaw === i) ||
+        (!origin && i >= parseInt(unit.initiativeRaw - factionStratAbility) &&
+        i <= parseInt(unit.initiativeRaw + factionStratAbility))
+      )
+
+      unitImages = unitTypes
+        .map(type => {
+          const [factionCode, unitCode] = type.split('-')
+          const unitGroup = units.filter(u => u.factionCode === factionCode && u.unitCode === unitCode)
+
+          const matchingUnit = unitGroup.find(u => origin ? u.initiativeRaw === i : u.initiative === i)
+
+          if (matchingUnit) {
+            return (
+              <UnitIcon
+                className={'initiative-row-image'}
+                tooltip={`${matchingUnit.name}\n${matchingUnit.men} Men`}
+                unitIconName={matchingUnit.iconName}
+                factionIconName={factions.find(f => f.code === matchingUnit.factionCode).icon}
+                veterancyIconName={veterancyMap[matchingUnit.veterancy].iconName}
+                highlighted={`${matchingUnit.factionCode}-${matchingUnit.unitCode}` === currentUnitType}
+              />
+            )
+          } else {
+            return null
+          }
+        })
+        .filter(icon => icon !== null)
 
       tableRows.push(
         <tr
           key={i}
           className={`${i % 2 === 0 ? 'even' : 'odd'}`}
-          onClick={origin ? () => {} : () => assignNewInitiative(i)}
+          onClick={origin ? () => {} : isHighlighted ? () => assignNewInitiative(i) : () => {}}
         >
           <td className='row-header'>{i}</td>
           <td className={`row-units ${isHighlighted ? 'highlighted' : ''}`}>{unitImages}</td>
@@ -125,21 +138,21 @@ const SetupInitiative = ({ users, units, setUnits, factions }) => {
   // RENDER
   return (
     <div className='setup-initiative'>
-      {currentUnitIndex !== -1 && (
+      {currentUnitTypeIndex !== -1 && (
         <div className='current-unit'>
           <div>
-            <img src={require(`../images/${factions.find(f => f.code === units[currentUnitIndex].factionCode).icon}`)} alt='' height={36} width={60} />
+            <img src={require(`../images/${factions.find(f => f.code === unitTypes[currentUnitTypeIndex]?.split('-')[0]).icon}`)} alt='' height={36} width={60} />
           </div>
-          <div>{factions.find(f => f.code === units[currentUnitIndex].factionCode).name}</div>
+          <div>{factions.find(f => f.code === unitTypes[currentUnitTypeIndex]?.split('-')[0]).name}</div>
           <div>
-            <img src={require(`../images/${units[currentUnitIndex].iconName}`)} alt='' height={60} width={60} />
+            <img src={require(`../images/${units.find(u => `${u.factionCode}-${u.unitCode}` === unitTypes[currentUnitTypeIndex])?.iconName}`)} alt='' height={60} width={60} />
           </div>
-          {/* <div>{units[currentUnitIndex]?.factionCode}-{units[currentUnitIndex]?.unitCode}-{units[currentUnitIndex]?.identifier}</div> */}
-          <div>{units[currentUnitIndex]?.name} {units[currentUnitIndex]?.identifier}</div>
+          <div>
+            {units.find(u => `${u.factionCode}-${u.unitCode}` === unitTypes[currentUnitTypeIndex])?.name}</div>
           <div>
             Users:
             {users.map(u => {
-              if (u.faction === units[currentUnitIndex].factionCode) {
+              if (u.faction === unitTypes[currentUnitTypeIndex]?.split('-')[0]) {
                 return (<span>{u.username}</span>)
               }
             })}
