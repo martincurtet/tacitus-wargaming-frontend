cb07f15cf1922069c43abd02d5b3a654fcd39a21
martincurtet <55143244+martincurtet@users.noreply.github.com>
2024-08-29 17:24:21 -0400

    feat: download save
    - added button and socket event to download save as json

--- src/components/Log.js
+++ src/components/Log.js
@@ -1,20 +1,47 @@
 import React, { useEffect } from 'react'
-import { socket } from '../connections/socket'
-import '../styles/components/Log.css'
 import { formatTimestamp } from '../functions/functions'
 
-const Log = ({ log, setLog }) => {
-  // useEffect(() => {
-  //   socket.on('log-updated', (data) => {
-  //     setLog(data.log)
-  //   })
+import { socket } from '../connections/socket'
+import { useParams } from 'react-router-dom'
 
-  //   return () => {
-  //     socket.off('log-updated')
-  //   }
-  // }, [])
+import Button from './Button'
+import '../styles/components/Log.css'
+
+const Log = ({ log }) => {
+  //
+  const params = useParams()
+
+  const handleDownload = () => {
+    socket.emit('download-file', {
+      roomUuid: params.battleuuid
+    })
+  }
+
+  useEffect(() => {
+    socket.on('file-downloaded', (data) => {
+      const jsonData = JSON.stringify(data.room)
+      const blob = new Blob([jsonData], { type: 'application/json' })
+      const url = URL.createObjectURL(blob)
+      const link = document.createElement('a')
+      link.download = 'data.json'
+      link.href = url
+      link.click()
+      URL.revokeObjectURL(url)
+    })
+
+    return () => {
+      socket.off('file-downloaded')
+    }
+  }, [])
+
+  // RENDER
   return (
     <div className='log'>
+      <Button
+        onClick={handleDownload}
+      >
+        Download
+      </Button>
       {log.map(l => (
         <p>[{formatTimestamp(l.timestamp, 'hh:min:ss')}] {l.log}</p>
       ))}
