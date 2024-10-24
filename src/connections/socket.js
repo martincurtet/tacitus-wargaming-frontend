e37e1bc6bec46aadc73bae615bb4d1aa36d76ff0
martincurtet <55143244+martincurtet@users.noreply.github.com>
2024-10-24 17:12:33 -0400

    feat: env
    - added environment variable check for server url

--- src/connections/socket.js
+++ src/connections/socket.js
@@ -1,6 +1,8 @@
 import { io } from 'socket.io-client'
 
-const URL = 'http://localhost:8080'
+const ENV = process.env.REACT_APP_ENVIRONMENT
+
+const URL = ENV === 'PROD' ? process.env.REACT_APP_SERVER_URL_PROD : process.env.REACT_APP_SERVER_URL_DEV
 
 export const socket = io(URL, {
   autoConnect: false
